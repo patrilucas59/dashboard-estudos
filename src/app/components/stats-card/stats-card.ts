c75874469382d01a6d780104c09700e2e84e98cf
Lucas Patrício <patriciolucas059@gmail.com>
2025-12-03 22:41:42 -0300

    feat(Home): componente stats-card com estrutura inicial

--- src/app/components/stats-card/stats-card.ts
+++ src/app/components/stats-card/stats-card.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-stats-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './stats-card.html',
   styleUrl: './stats-card.css',
 })
 export class StatsCard {
 
+  @Input() title: string = ''; // Nome da métrica (horas estudadas)
+  @Input() value: string | number = ''; // Valor atual (12h)
+  @Input() icon: string = ''; // Ícone (lucide)
+  @Input() color: string = '#4B7BFF'; // Cor do card
+  @Input() description?: string; // Descrição opicional
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { StatsCard } from '../../components/stats-card/stats-card';
+import { LucideAngularComponent, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [StatsCard, LucideAngularModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
 })
+
 export class Home {
 
+  kpis= [
+    { title: 'Horas Estudadas', value: '12h', icon: 'clock' },
+    { title: 'Matérias Concluídas', value: 3, icon: 'check-circle' },
+    { title: 'Progresso semanal', value: '64%', icon: 'line-chart' },
+    { title: 'Frequência', value: '4 dias', icon: 'calendar' },
+  ];
+
+  recentStudies = [
+    {
+      title: 'Matemática',
+      subtitle: 'Logaritmos',
+      progress: 65,
+      status: 'Em andamento,'
+    },
+    {
+      title: 'História',
+      subtitle: 'Segunda Guerra Mundial',
+      progress: 20,
+      status: 'Pendente',
+    }
+  ];
+
+  nextActivities = [
+    {
+      subject: 'Física',
+      description: 'Primeira Lei de Newton',
+      dueDate: new Date(),
+    }
+  ];
 }
